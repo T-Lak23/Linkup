e84ba11e6ca862b0dd819ab4a704bbe000b9ed54
T-Lak23 <tilak230412@gmail.com>
2025-10-23 13:45:05 +0530

    inngest fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import { ENV } from "./config/env.js";
 import { connectDb } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { serve } from "inngest/express";
-import { inngest, functions } from "./src/inngest";
+import { inngest, functions } from "./config/inngest.js";
 
 const app = express();
 
